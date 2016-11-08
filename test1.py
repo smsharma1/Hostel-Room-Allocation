9f0f8db50f7ede31534132c360c8f8baf000dcd6
smsharma1 <smsharma@iitk.ac.in>
2016-11-08 16:41:15 +0530

    some new technique

---
+++ test1.py
@@ -0,0 +1,105 @@
+import copy
+ 
+guyprefers = {
+ '1':  ['2', '3', '4', '5', '1'],
+ '2':  ['3', '4', '5', '1', '2'],
+ '3':  ['4', '5', '1', '2', '3'],
+ '4':  ['5', '1', '2', '3', '4'],
+ '5':   ['1', '2', '3', '4', '5']}
+galprefers = {
+ 'abi':  ['bob', 'fred', 'jon', 'gav', 'ian', 'abe', 'dan', 'ed', 'col', 'hal'],
+ 'bea':  ['bob', 'abe', 'col', 'fred', 'gav', 'dan', 'ian', 'ed', 'jon', 'hal'],
+ 'cath': ['fred', 'bob', 'ed', 'gav', 'hal', 'col', 'ian', 'abe', 'dan', 'jon'],
+ 'dee':  ['fred', 'jon', 'col', 'abe', 'ian', 'hal', 'gav', 'dan', 'bob', 'ed'],
+ 'eve':  ['jon', 'hal', 'fred', 'dan', 'abe', 'gav', 'col', 'ed', 'ian', 'bob'],
+ 'fay':  ['bob', 'abe', 'ed', 'ian', 'jon', 'dan', 'fred', 'gav', 'col', 'hal'],
+ 'gay':  ['jon', 'gav', 'hal', 'fred', 'bob', 'abe', 'col', 'ed', 'dan', 'ian'],
+ 'hope': ['gav', 'jon', 'bob', 'abe', 'ian', 'dan', 'hal', 'ed', 'col', 'fred'],
+ 'ivy':  ['ian', 'col', 'hal', 'gav', 'fred', 'bob', 'abe', 'ed', 'jon', 'dan'],
+ 'jan':  ['ed', 'hal', 'gav', 'abe', 'bob', 'jon', 'col', 'ian', 'fred', 'dan']}
+ 
+guys = sorted(guyprefers.keys())
+gals = sorted(guyprefers.keys())
+ 
+print guys
+print gals 
+def check(engaged):
+    inverseengaged = dict((v,k) for k,v in engaged.items())
+    for she, he in engaged.items():
+        shelikes = galprefers[she]
+        shelikesbetter = shelikes[:shelikes.index(he)]
+        helikes = guyprefers[he]
+        helikesbetter = helikes[:helikes.index(she)]
+        for guy in shelikesbetter:
+            guysgirl = inverseengaged[guy]
+            guylikes = guyprefers[guy]
+            if guylikes.index(guysgirl) > guylikes.index(she):
+                print("%s and %s like each other better than "
+                      "their present partners: %s and %s, respectively"
+                      % (she, guy, he, guysgirl))
+                return False
+        for gal in helikesbetter:
+            girlsguy = engaged[gal]
+            gallikes = galprefers[gal]
+            if gallikes.index(girlsguy) > gallikes.index(he):
+                print("%s and %s like each other better than "
+                      "their present partners: %s and %s, respectively"
+                      % (he, gal, she, girlsguy))
+                return False
+    return True
+ 
+def matchmaker():
+    guysfree = guys[:]
+    engaged  = {}
+    guyprefers2 = copy.deepcopy(guyprefers)
+    galprefers2 = copy.deepcopy(guyprefers)
+    while guysfree:
+        guy = guysfree.pop(0)
+        guyslist = guyprefers2[guy]
+        gal = guyslist.pop(0)
+	wif = engaged.get(guy)
+        fiance = engaged.get(gal)
+        if not fiance and not wif:
+	    print fiance,wif	
+            # She's free
+            engaged[gal] = guy
+            engaged[guy] = gal
+	    print engaged
+            print("  %s and %s" % (guy, gal))
+        else:
+            # The bounder proposes to an engaged lass!
+            galslist = galprefers2[gal]
+            if galslist.index(fiance) > galslist.index(guy):
+                # She prefers new guy
+                engaged[gal] = guy
+	        engaged[guy] = gal
+		del engaged[fiance]
+                print("  %s dumped %s for %s" % (gal, fiance, guy))
+                if guyprefers2[fiance]:
+                    # Ex has more girls to try
+                    guysfree.append(fiance)
+            else:
+                # She is faithful to old fiance
+                if guyslist:
+                    # Look again
+                    guysfree.append(guy)
+    return engaged
+ 
+ 
+print('\nEngagements:')
+engaged = matchmaker()
+ 
+print('\nCouples:')
+print('  ' + ',\n  '.join('%s is engaged to %s' % couple
+                          for couple in sorted(engaged.items())))
+print()
+print('Engagement stability check PASSED'
+      if check(engaged) else 'Engagement stability check FAILED')
+ 
+print('\n\nSwapping two fiances to introduce an error')
+engaged[gals[0]], engaged[gals[1]] = engaged[gals[1]], engaged[gals[0]]
+for gal in gals[:2]:
+    print('  %s is now engaged to %s' % (gal, engaged[gal]))
+print()
+print('Engagement stability check PASSED'
+      if check(engaged) else 'Engagement stability check FAILED')
