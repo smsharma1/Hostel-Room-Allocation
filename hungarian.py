f2ef8bdddfc0651279f747f947e3c39f587a02cd
smsharma1 <smsharma@iitk.ac.in>
2016-11-09 23:26:08 +0530

    I have fixed and finalized hungarian

--- hungarian.py
+++ hungarian.py
@@ -26,12 +26,8 @@ for document in cursor:
 		continue
 for param in temp:            
         room.append(param[0])
-print room
 pair=[]
 guys1=guys
-print guys
-print roomie
-print priority
 t = dict()
 matrix=[]
 for guy in guys1:
@@ -44,23 +40,15 @@ for guy in guys1:
                 t[guy] = map(add,priority[guy],priority[partner])
 	else:
 		continue	
-print matrix
-print t
-print pair 
 m = Munkres()
 indexes = m.compute(matrix)
-print_matrix(matrix, msg='Lowest cost through this matrix:')
-total = 0
 i = 0
 myroom = dict()
 for row, column in indexes:
     value = matrix[row][column]
-    total += value
-    print '(%s, %s) -> %s' % (pair[i][0], pair[i][1], room[column])
     myroom[pair[i][0]]=room[column]
     myroom[pair[i][1]]=room[column]
     i += 1
-print 'total cost: %d' % total
 cursor = db.users.find()
 for document in cursor:
     name = document["username"]
